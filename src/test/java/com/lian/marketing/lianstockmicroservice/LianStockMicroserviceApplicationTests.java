3419792263f973d7817746ff380230b7fce45bf3
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-09 13:53:38 -0500

    Project init

---
+++ src/test/java/com/lian/marketing/lianstockmicroservice/LianStockMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lian.marketing.lianstockmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LianStockMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
