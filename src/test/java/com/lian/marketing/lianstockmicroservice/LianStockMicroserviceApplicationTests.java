b825f6f3f924a754c7a24d3ae6b5673d2f28c2b1
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-11 18:46:51 -0500

    feat(properties): set test profile to contextLoads

--- src/test/java/com/lian/marketing/lianstockmicroservice/LianStockMicroserviceApplicationTests.java
+++ src/test/java/com/lian/marketing/lianstockmicroservice/LianStockMicroserviceApplicationTests.java
@@ -2,8 +2,10 @@ package com.lian.marketing.lianstockmicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LianStockMicroserviceApplicationTests {
 
     @Test
