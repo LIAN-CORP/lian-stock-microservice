8d7f28a758b39223d2c5281c66781a86aa318f96
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-11 19:45:04 -0500

    refactor(test): refactor contextLoads test

--- src/test/java/com/lian/marketing/lianstockmicroservice/LianStockMicroserviceApplicationTests.java
+++ src/test/java/com/lian/marketing/lianstockmicroservice/LianStockMicroserviceApplicationTests.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("No run contextLoads test")
 class LianStockMicroserviceApplicationTests {
 
-    @Disabled("No run contextLoads test")
     @Test
     void contextLoads() {
     }
