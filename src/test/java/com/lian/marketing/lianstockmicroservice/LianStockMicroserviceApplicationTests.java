f3def45fcc8ec1c0231a7515c1af96880f741e69
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-11 19:39:41 -0500

    refactor(test): refactor contextLoads test

--- src/test/java/com/lian/marketing/lianstockmicroservice/LianStockMicroserviceApplicationTests.java
+++ src/test/java/com/lian/marketing/lianstockmicroservice/LianStockMicroserviceApplicationTests.java
@@ -2,16 +2,8 @@ package com.lian.marketing.lianstockmicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
-@TestPropertySource(properties = {
-        "spring.datasource.url=",
-        "spring.datasource.username=",
-        "spring.datasource.password="
-})
 class LianStockMicroserviceApplicationTests {
 
     @Test
