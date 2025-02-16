6fd6caaf69486cb3ce8c2e2cf4b196ac93084940
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-16 18:23:43 -0500

    feta(infra): add method to handle argument not valid exception

--- src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driving/http/exceptionhandler/CategoryControllerAdvisor.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driving/http/exceptionhandler/CategoryControllerAdvisor.java
@@ -3,6 +3,7 @@ package com.lian.marketing.lianstockmicroservice.infrastructure.driving.http.exc
 import com.lian.marketing.lianstockmicroservice.domain.exception.CategoryAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +13,16 @@ import java.time.LocalDateTime;
 public class CategoryControllerAdvisor {
     @ExceptionHandler(CategoryAlreadyExistsException.class)
     public ResponseEntity<ExceptionResponse> handleCategoryAlreadyExistsException(CategoryAlreadyExistsException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(
+                HttpStatus.BAD_REQUEST.toString(),
+                HttpStatus.BAD_REQUEST.value(),
+                e.getMessage(),
+                LocalDateTime.now()
+        ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(
                 HttpStatus.BAD_REQUEST.toString(),
                 HttpStatus.BAD_REQUEST.value(),
