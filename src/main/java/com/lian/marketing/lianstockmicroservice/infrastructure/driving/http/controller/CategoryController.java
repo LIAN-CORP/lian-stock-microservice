0da99c0a0806c38e4b50e5b1cb3898ffaca3cef3
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-16 18:23:05 -0500

    refactor(infra): modify request mapping to correct url

--- src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driving/http/controller/CategoryController.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driving/http/controller/CategoryController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/category/")
+@RequestMapping("/category")
 @RequiredArgsConstructor
 public class CategoryController {
 
