833abf4aa163daf8420dbf50f2f0d4e6f59b1762
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-16 18:24:23 -0500

    refactor(infra): modify not null to not empty annotation to strings

--- src/main/java/com/lian/marketing/lianstockmicroservice/application/dto/request/CreateCategoryRequest.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/application/dto/request/CreateCategoryRequest.java
@@ -1,15 +1,15 @@
 package com.lian.marketing.lianstockmicroservice.application.dto.request;
 
 import com.lian.marketing.lianstockmicroservice.application.constants.RequestConstants;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public record CreateCategoryRequest(
-        @NotNull(message = RequestConstants.CATEGORY_NAME_MUST_NOT_BE_NULL)
+        @NotEmpty(message = RequestConstants.CATEGORY_NAME_MUST_NOT_BE_NULL)
         @Size(max = RequestConstants.CATEGORY_NAME_MAX_LENGTH)
         String name,
 
-        @NotNull(message = RequestConstants.CATEGORY_DESCRIPTION_MUST_NOT_BE_NULL)
+        @NotEmpty(message = RequestConstants.CATEGORY_DESCRIPTION_MUST_NOT_BE_NULL)
         @Size(max = RequestConstants.CATEGORY_DESCRIPTION_MAX_LENGTH)
         String description
 ) {
