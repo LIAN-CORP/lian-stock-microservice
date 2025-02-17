bbb55ba8dd1cdf1abe1fb56e02041e2489b1c1dd
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-16 20:21:49 -0500

    feat(app): add method to return all categories paged

--- src/main/java/com/lian/marketing/lianstockmicroservice/application/handler/CategoryHandler.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/application/handler/CategoryHandler.java
@@ -3,6 +3,8 @@ package com.lian.marketing.lianstockmicroservice.application.handler;
 import com.lian.marketing.lianstockmicroservice.application.dto.request.CreateCategoryRequest;
 import com.lian.marketing.lianstockmicroservice.application.mapper.ICategoryMapper;
 import com.lian.marketing.lianstockmicroservice.domain.api.ICategoryServicePort;
+import com.lian.marketing.lianstockmicroservice.domain.model.Category;
+import com.lian.marketing.lianstockmicroservice.domain.model.ContentPage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,9 @@ public class CategoryHandler {
 
     public void saveCategory(CreateCategoryRequest request) {
         categoryServicePort.createCategory(categoryMapper.fromRequestToDtoCategory(request));
+    }
+
+    public ContentPage<Category> findAllCategories(int page, int size, boolean isAsc) {
+        return categoryServicePort.findAllCategories(page, size, isAsc);
     }
 }
