56a6b77ba00e64fe91d30fe60a2fd211f36eb8cc
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-16 20:55:18 -0500

    refactor(app): change some status codes

--- src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driving/http/constants/OpenApiConstants.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driving/http/constants/OpenApiConstants.java
@@ -7,15 +7,16 @@ public class OpenApiConstants {
     public static final String CREATED_STATUS_CODE = "201";
     public static final String OK_STATUS_CODE = "200";
     public static final String BAD_REQUEST_STATUS_CODE = "400";
+    public static final String NOT_FOUND_STATUS_CODE = "404";
     public static final String MEDIATYPE_JSON = "application/json";
 
     //Category
     public static final String CATEGORY_POST_SUMMARY = "Save new category";
     public static final String CATEGORY_DESCRIPTION_201 = "Category saved";
     public static final String CATEGORY_DESCRIPTION_400 = "Invalid category";
+    public static final String CATEGORY_DESCRIPTION_404 = "Category or categories records not found";
     public static final String CATEGORIES_GET_ALL_SUMMARY = "Get all categories paged by page, size and is ascending params";
     public static final String CATEGORIES_GET_ALL_DESCRIPTION_201 = "Found categories records";
-    public static final String CATEGORIES_GET_ALL_DESCRIPTION_400 = "No categories records";
 
     //Paged
     public static final String CATEGORIES_DEFAULT_PAGE = "0";
