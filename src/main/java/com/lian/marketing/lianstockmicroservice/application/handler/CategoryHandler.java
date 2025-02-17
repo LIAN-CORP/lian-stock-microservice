ee10eec3a5c69cc3b9991da6268c6c3880d5e111
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-16 20:40:19 -0500

    feat(app): add method to find all categories

--- src/main/java/com/lian/marketing/lianstockmicroservice/application/handler/CategoryHandler.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/application/handler/CategoryHandler.java
@@ -1,12 +1,15 @@
 package com.lian.marketing.lianstockmicroservice.application.handler;
 
 import com.lian.marketing.lianstockmicroservice.application.dto.request.CreateCategoryRequest;
+import com.lian.marketing.lianstockmicroservice.application.dto.response.CategoryResponse;
 import com.lian.marketing.lianstockmicroservice.application.mapper.ICategoryMapper;
 import com.lian.marketing.lianstockmicroservice.domain.api.ICategoryServicePort;
 import com.lian.marketing.lianstockmicroservice.domain.model.Category;
 import com.lian.marketing.lianstockmicroservice.domain.model.ContentPage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +21,17 @@ public class CategoryHandler {
         categoryServicePort.createCategory(categoryMapper.fromRequestToDtoCategory(request));
     }
 
-    public ContentPage<Category> findAllCategories(int page, int size, boolean isAsc) {
-        return categoryServicePort.findAllCategories(page, size, isAsc);
+    public ContentPage<CategoryResponse> findAllCategories(int page, int size, boolean isAsc) {
+        ContentPage<Category> contentPage = categoryServicePort.findAllCategories(page, size, isAsc);
+        List<CategoryResponse> categoryResponseList = categoryMapper.toListCategoryResponse(contentPage.getContent());
+        return new ContentPage<>(
+                contentPage.getTotalPage(),
+                contentPage.getTotalElements(),
+                contentPage.getPageNumber(),
+                contentPage.getPageSize(),
+                contentPage.isFirst(),
+                contentPage.isLast(),
+                categoryResponseList
+        );
     }
 }
