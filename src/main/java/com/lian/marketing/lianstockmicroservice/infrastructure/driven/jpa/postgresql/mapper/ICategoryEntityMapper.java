b4816dc8b7492d18e6038f02260108763981859c
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-16 20:18:50 -0500

    feat(infra): add method to map list

--- src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driven/jpa/postgresql/mapper/ICategoryEntityMapper.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driven/jpa/postgresql/mapper/ICategoryEntityMapper.java
@@ -4,7 +4,10 @@ import com.lian.marketing.lianstockmicroservice.domain.model.Category;
 import com.lian.marketing.lianstockmicroservice.infrastructure.driven.jpa.postgresql.entity.CategoryEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ICategoryEntityMapper {
     CategoryEntity fromModelToCategoryEntity(Category category);
+    List<Category> fromCategoryEntityToCategoryList(List<CategoryEntity> categoryEntities);
 }
