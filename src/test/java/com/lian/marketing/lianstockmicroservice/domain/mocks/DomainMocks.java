e8d0502cdbe68d0eb11d0884a49ffd7b55027544
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-16 19:52:56 -0500

    feat(test): add new mocks to test

--- src/test/java/com/lian/marketing/lianstockmicroservice/domain/mocks/DomainMocks.java
+++ src/test/java/com/lian/marketing/lianstockmicroservice/domain/mocks/DomainMocks.java
@@ -1,7 +1,9 @@
 package com.lian.marketing.lianstockmicroservice.domain.mocks;
 
 import com.lian.marketing.lianstockmicroservice.domain.model.Category;
+import com.lian.marketing.lianstockmicroservice.domain.model.ContentPage;
 
+import java.util.List;
 import java.util.UUID;
 
 public class DomainMocks {
@@ -13,6 +15,22 @@ public class DomainMocks {
                 UUID.randomUUID(),
                 "Comida",
                 "Todo lo referente a comida"
+        );
+    }
+
+    public static List<Category> mockCategoriesList() {
+        return List.of(mockNormalCategory());
+    }
+
+    public static ContentPage<Category> mockCategoryPage() {
+        return new ContentPage<>(
+            1,
+                1,
+                0,
+                1,
+                true,
+                true,
+                mockCategoriesList()
         );
     }
 
