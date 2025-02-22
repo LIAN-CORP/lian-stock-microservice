9a73dc7a73105925d6a1c2ed4d95a543bf2b0c78
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-22 08:06:14 -0500

    feat(infra, domain): implement methods to subcategory exists by uuid

--- src/main/java/com/lian/marketing/lianstockmicroservice/domain/api/ISubcategoryServicePort.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/domain/api/ISubcategoryServicePort.java
@@ -3,7 +3,10 @@ package com.lian.marketing.lianstockmicroservice.domain.api;
 import com.lian.marketing.lianstockmicroservice.domain.model.ContentPage;
 import com.lian.marketing.lianstockmicroservice.domain.model.Subcategory;
 
+import java.util.UUID;
+
 public interface ISubcategoryServicePort {
     void createSubcategory(Subcategory subcategory);
     ContentPage<Subcategory> findAllSubcategories(int page, int size, boolean isAsc, String sortBy);
+    void subcategoryExistsByUUID(UUID uuid);
 }
--- src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driven/jpa/postgresql/adapter/SubcategoryAdapter.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driven/jpa/postgresql/adapter/SubcategoryAdapter.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 public class SubcategoryAdapter implements ISubcategoryPersistencePort {
@@ -48,5 +49,10 @@ public class SubcategoryAdapter implements ISubcategoryPersistencePort {
                 subcategoryPage.isLast(),
                 subcategoryList
         );
+    }
+
+    @Override
+    public boolean subcategoryExistsByUUID(UUID uuid) {
+        return subcategoryRepository.existsById(uuid);
     }
 }
