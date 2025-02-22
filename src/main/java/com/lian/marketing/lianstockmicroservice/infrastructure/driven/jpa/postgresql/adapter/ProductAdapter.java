8df0edcccdaf0e5a3bf5c33d67c5a52ea3d1ea9f
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-22 13:33:23 -0500

    feat(infra): add pageables methods and mapper

--- src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driven/jpa/postgresql/adapter/ProductAdapter.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/infrastructure/driven/jpa/postgresql/adapter/ProductAdapter.java
@@ -3,9 +3,17 @@ package com.lian.marketing.lianstockmicroservice.infrastructure.driven.jpa.postg
 import com.lian.marketing.lianstockmicroservice.domain.model.ContentPage;
 import com.lian.marketing.lianstockmicroservice.domain.model.Product;
 import com.lian.marketing.lianstockmicroservice.domain.spi.IProductPersistencePort;
+import com.lian.marketing.lianstockmicroservice.infrastructure.driven.jpa.postgresql.entity.ProductEntity;
 import com.lian.marketing.lianstockmicroservice.infrastructure.driven.jpa.postgresql.mapper.IProductEntityMapper;
 import com.lian.marketing.lianstockmicroservice.infrastructure.driven.jpa.postgresql.repository.IProductRepository;
+import com.lian.marketing.lianstockmicroservice.infrastructure.driven.jpa.postgresql.util.AdapterConstants;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class ProductAdapter implements IProductPersistencePort {
@@ -20,6 +28,21 @@ public class ProductAdapter implements IProductPersistencePort {
 
     @Override
     public ContentPage<Product> findAllProducts(int page, int size, boolean isAsc, String sortBy) {
-        return null;
+        Sort sort = isAsc
+                ? Sort.by(AdapterConstants.getValueSortMappingProducts(sortBy)).ascending()
+                : Sort.by(AdapterConstants.getValueSortMappingProducts(sortBy)).descending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<ProductEntity> products = productRepository.findAll(pageable);
+        List<Product> productList = productEntityMapper.toProductList(products.getContent());
+        return new ContentPage<>(
+                products.getTotalPages(),
+                products.getTotalElements(),
+                products.getPageable().getPageNumber(),
+                products.getPageable().getPageSize(),
+                products.isFirst(),
+                products.isLast(),
+                productList
+        );
     }
 }
