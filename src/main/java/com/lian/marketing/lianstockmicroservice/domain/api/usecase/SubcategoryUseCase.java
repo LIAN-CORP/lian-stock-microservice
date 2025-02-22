17e76e4d10a948ca819b2d73c73a3b3761ff9372
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-21 21:59:07 -0500

    feat(domain): add logic to find all subcategories in domain layer

--- src/main/java/com/lian/marketing/lianstockmicroservice/domain/api/usecase/SubcategoryUseCase.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/domain/api/usecase/SubcategoryUseCase.java
@@ -2,7 +2,9 @@ package com.lian.marketing.lianstockmicroservice.domain.api.usecase;
 
 import com.lian.marketing.lianstockmicroservice.domain.api.ISubcategoryServicePort;
 import com.lian.marketing.lianstockmicroservice.domain.constants.ExceptionConstants;
+import com.lian.marketing.lianstockmicroservice.domain.exception.SubcategoriesNotFoundException;
 import com.lian.marketing.lianstockmicroservice.domain.exception.SubcategoryAlreadyExistsException;
+import com.lian.marketing.lianstockmicroservice.domain.model.ContentPage;
 import com.lian.marketing.lianstockmicroservice.domain.model.Subcategory;
 import com.lian.marketing.lianstockmicroservice.domain.spi.ISubcategoryPersistencePort;
 import lombok.RequiredArgsConstructor;
@@ -18,5 +20,14 @@ public class SubcategoryUseCase implements ISubcategoryServicePort {
             throw new SubcategoryAlreadyExistsException(String.format(ExceptionConstants.SUBCATEGORY_ALREADY_EXISTS_WITH_NAME, subcategory.getName()));
         }
         subcategoryPersistencePort.saveSubcategory(subcategory);
+    }
+
+    @Override
+    public ContentPage<Subcategory> findAllSubcategories(int page, int size, boolean isAsc, String sortBy) {
+        ContentPage<Subcategory> content = subcategoryPersistencePort.findAllSubcategory(page, size, isAsc, sortBy);
+        if(content.getContent().isEmpty()) {
+            throw new SubcategoriesNotFoundException(ExceptionConstants.NO_RECORDS_FOR_SUBCATEGORIES);
+        }
+        return content;
     }
 }
--- src/main/java/com/lian/marketing/lianstockmicroservice/domain/constants/ExceptionConstants.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/domain/constants/ExceptionConstants.java
@@ -7,4 +7,5 @@ public class ExceptionConstants {
     public static final String NO_RECORDS_FOR_CATEGORIES = "No records for categories";
 
     public static final String SUBCATEGORY_ALREADY_EXISTS_WITH_NAME = "Subcategory already exists with name: %s";
+    public static final String NO_RECORDS_FOR_SUBCATEGORIES = "No records for subcategories";
 }
