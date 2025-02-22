f5bc95defeae77539e8ca68b707f66a8ef40701c
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-22 08:54:04 -0500

    feat(app): add dto request and constants for product

---
+++ src/main/java/com/lian/marketing/lianstockmicroservice/application/dto/request/CreateProductRequest.java
@@ -0,0 +1,30 @@
+package com.lian.marketing.lianstockmicroservice.application.dto.request;
+
+import com.lian.marketing.lianstockmicroservice.application.constants.RequestConstants;
+import jakarta.validation.constraints.*;
+
+public record CreateProductRequest(
+    @NotEmpty(message = RequestConstants.PRODUCT_NAME_MUST_NOT_BE_NULL)
+    @Size(max = RequestConstants.PRODUCT_NAME_MAX_LENGTH)
+    String name,
+
+    @NotEmpty(message = RequestConstants.PRODUCT_DESCRIPTION_MUST_NOT_BE_NULL)
+    @Size(max = RequestConstants.PRODUCT_DESCRIPTION_MAX_LENGTH)
+    String description,
+
+    @NotNull(message = RequestConstants.PRODUCT_STOCK_MUST_NOT_BE_NULL)
+    @PositiveOrZero(message = RequestConstants.PRODUCT_STOCK_MUST_BE_POSITIVE_OR_ZERO)
+    int stock,
+
+    @NotNull(message = RequestConstants.PRODUCT_PRICE_SELL_MUST_NOT_BE_NULL)
+    @Positive(message = RequestConstants.PRODUCT_PRICE_SELL_MUST_BE_POSITIVE)
+    double priceSell,
+
+    @NotNull(message = RequestConstants.PRODUCT_PRICE_BUY_MUST_NOT_BE_NULL)
+    @Positive(message = RequestConstants.PRODUCT_PRICE_BUY_MUST_BE_POSITIVE)
+    double priceBuy,
+
+    @NotEmpty(message = RequestConstants.SUBCATEGORY_ID_MUST_NOT_BE_NULL)
+    String subcategoryId
+) {
+}
