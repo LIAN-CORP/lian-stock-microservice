f0bd778c1af92e1f33e07da0fd03a31bcdb66bc2
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-13 10:11:56 -0500

    feat(app): add category and subcategory mapping in ProductResponse

--- src/main/java/com/lian/marketing/lianstockmicroservice/application/dto/response/ProductResponse.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/application/dto/response/ProductResponse.java
@@ -1,11 +1,16 @@
 package com.lian.marketing.lianstockmicroservice.application.dto.response;
 
+import java.util.UUID;
+
 public record ProductResponse(
+        UUID id,
         String name,
         String description,
         Integer stock,
         Double priceSell,
         Double priceBuy,
-        SubcategoryItem subcategory
+        String subcategory,
+        String category
+        //SubcategoryItem subcategory
 ) {
 }
