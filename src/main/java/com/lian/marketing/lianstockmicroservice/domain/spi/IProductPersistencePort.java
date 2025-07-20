9b90daf5d5ddb043e237b857409b21589abe64b2
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-19 22:21:38 -0500

    feat(domain): add product inventory validation and stock discount methods

--- src/main/java/com/lian/marketing/lianstockmicroservice/domain/spi/IProductPersistencePort.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/domain/spi/IProductPersistencePort.java
@@ -3,7 +3,13 @@ package com.lian.marketing.lianstockmicroservice.domain.spi;
 import com.lian.marketing.lianstockmicroservice.domain.model.ContentPage;
 import com.lian.marketing.lianstockmicroservice.domain.model.Product;
 
+import java.util.UUID;
+
 public interface IProductPersistencePort {
     void saveProduct(Product product);
     ContentPage<Product> findAllProducts(int page, int size, boolean isAsc, String sortBy);
+    boolean productExistsByUUID(UUID id);
+    boolean isProductInStock(UUID id, Integer quantity);
+    boolean isPriceSellValid(UUID id, Double priceSell);
+    void discountProductInStock(UUID id, Integer quantity);
 }
