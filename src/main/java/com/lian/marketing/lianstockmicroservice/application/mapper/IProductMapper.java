ed47f564786abab382c206b41bb199466ca3b6c7
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-20 00:32:44 -0500

    feat(config): enable SQL formatting and logging of Hibernate types in application properties

--- src/main/java/com/lian/marketing/lianstockmicroservice/application/mapper/IProductMapper.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/application/mapper/IProductMapper.java
@@ -1,6 +1,7 @@
 package com.lian.marketing.lianstockmicroservice.application.mapper;
 
 import com.lian.marketing.lianstockmicroservice.application.dto.request.CreateProductRequest;
+import com.lian.marketing.lianstockmicroservice.application.dto.request.DiscountProductStockRequest;
 import com.lian.marketing.lianstockmicroservice.application.dto.response.ProductResponse;
 import com.lian.marketing.lianstockmicroservice.domain.model.Product;
 import org.mapstruct.Mapper;
@@ -15,4 +16,15 @@ public interface IProductMapper {
     Product toModel(CreateProductRequest request);
     ProductResponse toResponse(Product product);
     List<ProductResponse> toResponseList(List<Product> products);
+
+    @Mapping(target = "name", ignore = true)
+    @Mapping(target = "description", ignore = true)
+    @Mapping(target = "stock", source = "quantity")
+    @Mapping(target = "priceSell",ignore = true)
+    @Mapping(target = "priceBuy", ignore = true)
+    @Mapping(target = "subcategory", ignore = true)
+    @Mapping(target = "imagePath", ignore = true)
+    Product toModelFromDiscountRequest(DiscountProductStockRequest request);
+
+    List<Product> toModelListFromDiscountListRequest(List<DiscountProductStockRequest> request);
 }
