05fc473d347f45850f1e9c8171a9b1de27d3d133
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-19 22:21:27 -0500

    feat(domain): add method to apply discounts to products in stock

--- src/main/java/com/lian/marketing/lianstockmicroservice/domain/api/IProductServicePort.java
+++ src/main/java/com/lian/marketing/lianstockmicroservice/domain/api/IProductServicePort.java
@@ -4,7 +4,10 @@ import com.lian.marketing.lianstockmicroservice.domain.model.ContentPage;
 import com.lian.marketing.lianstockmicroservice.domain.model.Product;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface IProductServicePort {
     void createProduct(Product product, MultipartFile file);
     ContentPage<Product> findAllProducts(int page, int size, boolean isAsc, String sortBy);
+    void discountProductsInStock(List<Product> product);
 }
